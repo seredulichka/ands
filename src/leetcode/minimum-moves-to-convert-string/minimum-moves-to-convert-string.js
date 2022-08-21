2f67a9c64c17105aa5f407fa39e5ec2eb7126fa0
seredulichka <alinasereda300898@gmail.com>
2022-08-21 10:20:36 +0100

    feat: faster and less solution minimum-moves-to-convert-string

--- src/leetcode/minimum-moves-to-convert-string/minimum-moves-to-convert-string.js
+++ src/leetcode/minimum-moves-to-convert-string/minimum-moves-to-convert-string.js
@@ -3,16 +3,30 @@
  * Difficulty: Easy
  * https://leetcode.com/problems/minimum-moves-to-convert-string/
  */
+// export const minimumMovesToConvertString = s => {
+//   let counter = 0;
+//   let i = 0;
+//   for (let i = 0; i < s.length; ) {
+//     if (s[i] === 'O') {
+//       i += 1;
+//     } else if (s[i] === 'X' || s[i + 1] === 'X' || s[i + 2] === 'X') {
+//       counter += 1;
+//       i += 3;
+//     }
+//   }
+
+//   return counter;
+// };
+
+//faster and less solution
+
 export const minimumMovesToConvertString = s => {
   let counter = 0;
   let i = 0;
   for (let i = 0; i < s.length; ) {
-    if (s[i] === 'O') {
-      i += 1;
-    } else if (s[i] === 'X' || s[i + 1] === 'X' || s[i + 2] === 'X') {
-      counter += 1;
-      i += 3;
-    }
+    const condition = s[i] === 'O';
+    counter += condition ? 0 : 1;
+    i += condition ? 1 : 3;
   }
 
   return counter;
