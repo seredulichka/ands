ff6cb7ce7f77f807a641d51fe2ea017936af41ab
seredulichka <alinasereda300898@gmail.com>
2022-08-21 18:55:46 +0100

    added missed tests for slowest-key'

--- src/leetcode/slowest-key/slowest-key.test.js
+++ src/leetcode/slowest-key/slowest-key.test.js
@@ -1,10 +1,14 @@
 import { slowestKey } from './slowest-key';
 describe('[Leetcode] slowest-key', () => {
   test('example #1', () => {
-    expect(slowestKey()).toBe('');
+    expect(slowestKey([9, 29, 49, 50], 'cbcd')).toBe('c');
   });
 
   test('example #2', () => {
-    expect(slowestKey()).toBe('');
+    expect(slowestKey([12, 23, 36, 46, 62], 'spuda')).toBe('a');
+  });
+
+  test('example #3', () => {
+    expect(slowestKey([23, 34, 43, 59, 62, 80, 83, 92, 97], 'qgkzzihfc')).toBe('q');
   });
 });
