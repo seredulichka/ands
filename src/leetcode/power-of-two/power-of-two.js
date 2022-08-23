e405782f951db1ddebd60a596a810fb4bfc02500
seredulichka <alinasereda300898@gmail.com>
2022-08-23 08:47:49 +0100

    feat: solve power-of-two

---
+++ src/leetcode/power-of-two/power-of-two.js
@@ -0,0 +1,12 @@
+/**
+ * Leetcode #231
+ * Difficulty: Easy
+ * https://leetcode.com/problems/power-of-two/
+ */
+export const powerOfTwo = n => {
+  while (n > 1) {
+    n /= 2;
+  }
+
+  return n === 1 ? true : false;
+};
