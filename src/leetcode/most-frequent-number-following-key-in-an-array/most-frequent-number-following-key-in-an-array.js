e723420f7e13abeebdd20df2f67b0eb9d0bf2d19
Alina <alinasereda300898@gmail.com>
2022-08-27 17:50:41 +0100

    feat: solve most-frequent-number-following-key-in-an-array again

--- src/leetcode/most-frequent-number-following-key-in-an-array/most-frequent-number-following-key-in-an-array.js
+++ src/leetcode/most-frequent-number-following-key-in-an-array/most-frequent-number-following-key-in-an-array.js
@@ -3,15 +3,36 @@
 * Difficulty: Easy
 * https://leetcode.com/problems/most-frequent-number-following-key-in-an-array/
 */
+// export const mostFrequentNumberFollowingKeyInAnArray = (nums, key) => {
+//     const map = new Map();
+    
+//     for (let i = 0; i < nums.length; i += 1) {
+//         if(nums[i] === key) {
+//             map.set(nums[i + 1], (map.get(nums[i + 1]) ?? 0) + 1)
+//         }
+//     }
+        
+//     const sortedArr = [...map].sort((a,b) => b[1] - a[1])
+//     return sortedArr[0][0]
+// };
+
 export const mostFrequentNumberFollowingKeyInAnArray = (nums, key) => {
     const map = new Map();
+    let maxCount = 0;
+    let maxValue = 0;
     
     for (let i = 0; i < nums.length; i += 1) {
-        if(nums[i] === key) {
-            map.set(nums[i + 1], (map.get(nums[i + 1]) ?? 0) + 1)
+        if (nums[i] === key) {
+            const target = nums[i + 1]
+            const count =  (map.get(target) ?? 0) + 1
+            map.set(target, count)
+            
+            if (count > maxCount) {
+                maxCount = count;
+                maxValue = target;
+            }
         }
     }
         
-    const sortedArr = [...map].sort((a,b) => b[1] - a[1])
-    return sortedArr[0][0]
+    return maxValue;
 };
