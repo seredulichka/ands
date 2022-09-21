2eea702b6dabd82b703f85e8b87b7a9b5afacf76
Alina <alinasereda300898@gmail.com>
2022-09-21 17:35:43 +0100

    feat: solve sort-array-by-parity again

--- src/leetcode/sort-array-by-parity/sort-array-by-parity.js
+++ src/leetcode/sort-array-by-parity/sort-array-by-parity.js
@@ -3,18 +3,29 @@
 * Difficulty: Easy
 * https://leetcode.com/problems/sort-array-by-parity/
 */
-export const sortArrayByParity = nums => {
-    let last = nums.length;
+// export const sortArrayByParity = nums => {
+//     let last = nums.length;
 
-    for (let i = 0; i < last;) {
-        if (nums[i] % 2 !== 0 ) {
-            nums.push(nums[i]);
-            nums.splice(i, 1);
-            last -= 1;
-        } else {
-            i += 1;
-        }
+//     for (let i = 0; i < last;) {
+//         if (nums[i] % 2 !== 0 ) {
+//             nums.push(nums[i]);
+//             nums.splice(i, 1);
+//             last -= 1;
+//         } else {
+//             i += 1;
+//         }
+//     }
+   
+//     return nums;
+// };
+
+export const sortArrayByParity = nums => {
+    let odd = [];
+    let even = []; 
+    
+    for (const num of nums) {
+        num % 2 === 0 ? even.push(num) : odd.push(num);
     }
    
-    return nums;
+    return even.concat(odd);
 };
